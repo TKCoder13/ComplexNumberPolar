5a665e70f862b1b2b04f1f5bc00ba0454a01dfff
TKCoder13 <TKCoding13@gmail.com>
2023-11-13 00:24:02 -0800

    Start CNP

--- ComplexNumberPolar.java
+++ ComplexNumberPolar.java
@@ -1,3 +1,19 @@
-public class ComplexNumberPolar {
+public class ComplexNumberPolar extends ComplexNumber {
     
+    private double radius;
+    private double theta;
+
+    public ComplexNumberPolar() {
+        radius = 0;
+        theta = 1;
+    }
+
+    public ComplexNumberPolar(double radius, double theta) {
+        this.radius = radius;
+        this.theta = theta;
+    }
+
+    public ComplexNumberPolar(ComplexNumber cn) {
+        super();
+    }
 }
